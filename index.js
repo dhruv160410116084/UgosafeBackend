dbba704673299ad477084edfc6242e79e9845d65
dhruv <dhruv.160410116084@gmail.com>
2019-11-01 22:49:08 +0530

    added PORT

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ db.once('open', function() {
   console.log('success');
 });
 
-app.listen(process.env.port || 3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log('server is listening on 3000');
 });
