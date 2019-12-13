36288b07f79c45c3f8bb4311c589cc3eede82cb4
dhruv <dhruv.160410116084@gmail.com>
2019-12-13 17:22:32 +0530

    change in get location api

--- Routes/location.js
+++ Routes/location.js
@@ -3,7 +3,7 @@ let locationContorller = require('../contorller/location');
 
 router.post('/',locationContorller.setLiveLocation);
 router.get('/panic/list',locationContorller.fetchPanicList);
-router.get('/:email',locationContorller.getLiveLocation);
+router.get('/live/:email',locationContorller.getLiveLocation);
 router.get('/panic/add/:email',locationContorller.panicAdd);
 router.get('/panic/remove/:email',locationContorller.panicRemove);
 
